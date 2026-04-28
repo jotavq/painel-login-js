a000f5b24102f0bcb2d04f1fb2fe5d1f05683a4f
”jotavq” <jottavz@icloud.com>
2026-04-27 22:32:47 -0300

    Atualização: Nova Interface, correção de tentativas, registro e lembrar senha adicionado

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ Projeto simples de login desenvolvido com HTML, CSS e JavaScript.
 
 ## Funcionalidades
 
-- Validação de email e senha
-- Controle de tentativas
-- Bloqueio após 3 erros
+- Login com validação de email e senha
+- Sistema de registro de usuário
+- Limite de tentativas de login
+- Bloqueio após múltiplas tentativas
+- Opção "lembrar senha" utilizando localStorage
+- Mensagens de feedback para o usuário
 
 ## Tecnologias
 
@@ -17,3 +20,13 @@ Projeto simples de login desenvolvido com HTML, CSS e JavaScript.
 ## Objetivo
 
 Projeto criado para praticar lógica de programação e manipulação de DOM.
+
+## Próximos passos
+
+- Integração com um dashboard
+- Melhorias na interface
+- Validações mais robustas
+
+## Preview
+
+![Preview do sistema de login](img/preview.png)
--- script.js
+++ script.js
@@ -1,21 +1,60 @@
 let email = document.querySelector('#txemail')
 let pass = document.querySelector ('#txsenha')
 let res = document.querySelector('#res')
+let lembrar = document.querySelector('#lembrar')
 let tentativas = 0
 
-function entrar() {
-    if (email.value === 'admin' && pass.value === '1234') {
-        res.innerHTML = 'Login bem sucedido'
-        res.style.color = 'green'
-    } else if (tentativas < 3) {
-        tentativas++
-        res.innerHTML = `Login falhou. Tentativa ${tentativas}`
-        res.style.color = 'red'
-    } else if (tentativas >= 3) {
-    res.innerHTML = 'Acesso bloqueado.'
-    res.style.color = 'red'
-    let botao = document.querySelector('input[type="button"]')
-    botao.disabled = true
-    }
+let emailCadastrado = ""
+let senhaCadastrada = ""
 
+function esqueciSenha() {
+    res.innerHTML = 'Função em desenvolvimento'
+    res.style.color = '#ff0000'
+}
+
+function registrar() {
+    emailCadastrado = email.value
+    senhaCadastrada = pass.value
+    res.innerHTML = 'Registro feito com Sucesso'
+    res.style.color = '#0FF0FC'
+}
+
+function entrar() {
+    if (emailCadastrado === "" || senhaCadastrada === "") {
+        res.innerHTML = 'Faça o registro antes de tentar entrar'
+        res.style.color = '#ff0000'
+        return
+    }
+    if (email.value === emailCadastrado && pass.value === senhaCadastrada) {
+        res.innerHTML = 'Login bem sucedido'
+        res.style.color = '#00ff00'
+    if (lembrar.checked) {
+        localStorage.setItem('email', email.value)
+        localStorage.setItem('senha', pass.value)
+    } else {
+        localStorage.removeItem('email')
+        localStorage.removeItem('senha')
+    }
+    } else {
+        tentativas++
+  if (tentativas >= 3) {
+        res.innerHTML = 'Acesso bloqueado.'
+        res.style.color = '#ff0000'
+
+        let botao = document.querySelector('input[type="button"]')
+        botao.disabled = true
+    } else {
+        res.innerHTML = `Login falhou, você tem mais <strong>${3 - tentativas} tentativas</strong>`
+        res.style.color = '#ff0000'
+    }
+}
+}
+
+let emailSalvo = localStorage.getItem('email')
+let senhaSalva = localStorage.getItem('senha')
+
+if (emailSalvo && senhaSalva) {
+    email.value = emailSalvo
+    pass.value = senhaSalva
+    lembrar.checked = true
 }
